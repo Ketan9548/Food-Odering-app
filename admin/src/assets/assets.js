de61e065eb6d4513c8d60818e6d0738dba0f93be
kapil <kc918062@gmail.com>
2024-07-24 11:17:25 +0530

    done

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,5 +14,5 @@ export const assets ={
     parcel_icon
 }
 
-// export const url = 'https://food-del-backend-tz8j.onrender.com'
-export const url = 'http://localhost:4500'
+export const url = 'https://food-del-backend-t5gh.onrender.com/'
+// export const url = 'http://localhost:4500'
