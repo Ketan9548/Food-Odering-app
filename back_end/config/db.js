7573f763c3e47722cf4249cc962947aff3bac696
kapil <kc918062@gmail.com>
2024-07-24 11:35:23 +0530

    done

--- back_end/config/db.js
+++ back_end/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://kapilchauhan221999:9113M7reTavVQPn1@cluster1.wveh7p7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1').then(()=>console.log("DB Connected"))
+    await mongoose.connect('mongodb://localhost:27017/food-del').then(()=>console.log("DB Connected"))
 }
