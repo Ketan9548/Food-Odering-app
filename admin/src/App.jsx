d1d4b6f2bd6ab311007253cc1d729d21e06bbd6a
kapil <kc918062@gmail.com>
2024-07-24 16:33:30 +0530

    done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from './pages/Orders/Orders'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
-  const url = "http://localhost:4500";
+  const url = "https://food-del-backend-5onc.onrender.com";
   return (
     <div>
       <ToastContainer />
